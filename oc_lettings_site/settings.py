fed890c283e74963f03d20dc4d6b69b202509ef9
JasonBats <46994866+JasonBats@users.noreply.github.com>
2024-09-15 14:58:28 +0200

    try to fix static folder not reachable

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 # Application definition
@@ -127,4 +127,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 print(STATIC_ROOT)
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static", ]
+# STATICFILES_DIRS = [BASE_DIR / "static"]
